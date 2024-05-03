f8e38de8299a95cb86c246fd19c782d6bf12eb4b
Kate Holdener <kate.holdener@slu.edu>
2024-05-03 11:36:37 -0500

    added test cases for the board, one of them fails

--- app/src/test/java/mvc/model/TicTacToeBoardTest.java
+++ app/src/test/java/mvc/model/TicTacToeBoardTest.java
@@ -7,9 +7,40 @@ public class TicTacToeBoardTest {
 
     @Test
     public void testPlacePiece(){
+        TicTacToeBoard board = new TicTacToeBoard();
+        boolean result = board.placeTicTacToePiece(0,0,TicTacToePiece.X);
+        assertTrue("Placing a piece in an empty location", result);
+        TicTacToePiece piece = board.getTicTacToePiece(0,0);
+        assertTrue("Expect that the piece is X", piece == TicTacToePiece.X);
     }
 
     @Test
     public void testPlacePieceSameLocation(){
+        TicTacToeBoard board = new TicTacToeBoard();
+        board.placeTicTacToePiece(0,0,TicTacToePiece.X);
+        boolean result = board.placeTicTacToePiece(0,0,TicTacToePiece.O);
+
+        assertFalse("Placing a piece in a non-empty location", result);
+        TicTacToePiece piece = board.getTicTacToePiece(0,0);
+        assertTrue("Expect that the piece is X", piece == TicTacToePiece.X);
     }
+
+    @Test
+    public void testPlacePieceOutsideOfBoard(){
+        TicTacToeBoard board = new TicTacToeBoard();
+        boolean result = board.placeTicTacToePiece(3,3,TicTacToePiece.X);
+        assertFalse("Placing a piece in non-existing location", result);
+    }
+
+    @Test
+    public void testWinnerHorizontalX(){
+        TicTacToeBoard board = new TicTacToeBoard();
+        board.placeTicTacToePiece(0,0,TicTacToePiece.X);
+        board.placeTicTacToePiece(0,1,TicTacToePiece.X);
+        board.placeTicTacToePiece(0,2,TicTacToePiece.X);
+
+        TicTacToePiece winner = board.getWinner();
+        assertEquals("Winner ", TicTacToePiece.X, winner);
+    }
+
 }
