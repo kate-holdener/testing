204c0376c18e19f57836130b8ee5d78656c4603e
Kate Holdener <kate.holdener@slu.edu>
2024-05-03 10:53:47 -0500

    added starter test code for model

---
+++ app/src/test/java/mvc/model/TicTacToeBoardTest.java
@@ -0,0 +1,15 @@
+package mvc.model;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TicTacToeBoardTest {
+
+    @Test
+    public void testPlacePiece(){
+    }
+
+    @Test
+    public void testPlacePieceSameLocation(){
+    }
+}
